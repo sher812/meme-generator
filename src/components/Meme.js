ccc81cf30f3fd0aeea926063fbdf84c739b4158f
sher812 <sher812@aucklanduni.ac.nz>
2022-08-24 21:57:29 +1200

    added button functionality

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,28 @@
-import React from "react"
+import React, {useState} from "react"
+import memesData from "../memesData";
 
+let url = ""
 export default function Meme() {
+
+    const [meme, getMeme] = useState("https://i.imgflip.com/30b1gx.jpg")
+
+    function handleClick() {
+        const memeLength= memesData.data.memes.length
+        const randMeme = Math.floor(Math.random()*memeLength)
+        const memeURL = memesData.data.memes[randMeme].url
+
+        console.log(meme)
+        getMeme(memeURL)
+    }
+
     return(
         <div className="meme">
             <form className="meme-form">
-                <input type="text" className="meme-top"/>
-                <input type="text" className="meme-bottom"/>
+                <input placeholder="Top text" type="text" className="meme-top"/>
+                <input placeholder="Bottom text" type="text" className="meme-bottom"/>
             </form>
-            <button> Create New Meme </button>
+            <button onClick={handleClick}> Get New Meme Image </button>
+            <img className="meme-img" src={meme}/>
         </div>
     )
 }
